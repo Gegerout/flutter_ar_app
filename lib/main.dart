5834e9f4201b0ac88ae68b37a1e03649b33f7f37
Gegerout <selimchik06@gmail.com>
2023-10-13 12:19:57 +0300

    AR widgets update

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         ),
         // body: ARKitSceneView(
         //     onARKitViewCreated: (controller) => arView(controller)),
-        body: const MyHomePage(title: "AR Widget"),
+        body: const WidgetProjectionPage(),
       ),
     );
   }
