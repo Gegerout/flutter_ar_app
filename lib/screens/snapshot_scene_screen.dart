e0b774ba31c8712ed61fe1787db5c2deba16d45d
Gegerout <selimchik06@gmail.com>
2023-10-21 23:50:13 +0300

    Snapshot scene screen update

---
+++ lib/screens/snapshot_scene_screen.dart
@@ -0,0 +1,82 @@
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/ar_helper.dart';
+
+class SnapshotScenePage extends StatefulWidget {
+  const SnapshotScenePage({super.key});
+
+  @override
+  State<SnapshotScenePage> createState() {
+    return _SnapshotScenePageState();
+  }
+}
+
+class _SnapshotScenePageState extends State<SnapshotScenePage> {
+  late ARKitController arkitController;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: const Text('Snapshot'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.camera_alt),
+        onPressed: () async {
+          try {
+            final image = await arkitController.snapshot();
+            if (context.mounted) {
+              await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SnapshotPreview(
+                  imageProvider: image,
+                ),
+              ),
+            );
+            }
+          } catch (e) {
+            if (kDebugMode) {
+              print(e);
+            }
+          }
+        },
+      ),
+      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    this.arkitController.add(createSphere());
+  }
+}
+
+class SnapshotPreview extends StatelessWidget {
+  const SnapshotPreview({
+    Key? key,
+    required this.imageProvider,
+  }) : super(key: key);
+
+  final ImageProvider imageProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Preview'),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image(image: imageProvider),
+        ],
+      ),
+    );
+  }
+}
