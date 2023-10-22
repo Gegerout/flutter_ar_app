7866b9052a8f9283fa8e83ed0d6a9ffa5109b755
Gegerout <selimchik06@gmail.com>
2023-10-23 00:16:35 +0300

    Video screen update

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:ar_app/screens/plane_detection_screen.dart';
 import 'package:ar_app/screens/real_time_updates_screen.dart';
 import 'package:ar_app/screens/snapshot_scene_screen.dart';
 import 'package:ar_app/screens/tap_screen.dart';
+import 'package:ar_app/screens/video_page.dart';
 import 'package:ar_app/screens/widget_projection_screen.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
@@ -43,7 +44,8 @@ class MyApp extends StatelessWidget {
         //body: const PlaneDetectionPage(),
         //body: const RealTimeUpdatesPage()
         //body: const SnapshotScenePage(),
-        body: const TapPage(),
+        //body: const TapPage(),
+        body: const VideoPage(),
       ),
     );
   }
