36103c46cc9d2c2331ef13c5a36870dd810d1bea
Gegerout <selimchik06@gmail.com>
2023-12-28 17:47:05 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("AR App"),
+        ),
+        body: ARKitSceneView(
+            onARKitViewCreated: (controller) => arView(controller)),
+      ),
+    );
+  }
+
+  void arView(ARKitController controller) {
+    final nodeAr = ARKitNode(
+        geometry: ARKitSphere(materials: [
+      ARKitMaterial(
+          diffuse: const ARKitMaterialImage("assets/world.jpg"),
+          doubleSided: true)
+    ], radius: 1), position: Vector3(0,0,0));
+    controller.add(nodeAr);
+  }
+}
