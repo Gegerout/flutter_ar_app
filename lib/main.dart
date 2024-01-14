a22a4e91e9bd40837b6f700e57f267705e3c8410
Gegerout <selimchik06@gmail.com>
2024-01-14 15:50:57 +0300

    colors update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter AR App',
       theme: ThemeData(
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF2C00))
       ),
       home: const HomeScreen(),
     );
