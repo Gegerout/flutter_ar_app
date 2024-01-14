9fb5f83f1390aa62447e48f3da8b968df80c2aa9
Gegerout <selimchik06@gmail.com>
2024-01-14 16:30:34 +0300

    colors update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ar_app/utils/screen_card.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../models/screen_model.dart';
 import 'change_icon_screen.dart';
 import 'earth_screen.dart';
@@ -173,17 +174,31 @@ class HomeScreen extends StatelessWidget {
         'Change icon',
         'Changes app icon',
         Icons.app_registration,
-            () => Navigator.of(context).push<void>(
+        () => Navigator.of(context).push<void>(
             MaterialPageRoute(builder: (c) => const ChangeIconScreen())),
       )
     ];
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AR App'),
+        title: Row(
+          children: [
+            SvgPicture.asset("assets/icon/icon_new_red.svg",
+              colorFilter:
+              const ColorFilter.mode(Color(0xFFFF2C00), BlendMode.srcIn),
+              semanticsLabel: 'A red up arrow', height: 32, width: 32,),
+            const SizedBox(width: 16,),
+            const Text('AR App')
+          ],
+        ),
       ),
-      body:
-          ListView(children: samples.map((s) => ScreenCard(item: s)).toList()),
+      body: ListView(
+          children: samples
+              .map((s) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ScreenCard(item: s),
+                  ))
+              .toList()),
     );
   }
 }
--- lib/utils/screen_card.dart
+++ lib/utils/screen_card.dart
@@ -10,21 +10,63 @@ class ScreenCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: InkWell(
-        onTap: () => item.onTap(),
-        child: ListTile(
-          leading: Icon(item.icon),
-          title: Text(
-            item.title,
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          subtitle: Text(
-            item.description,
-            style: Theme.of(context).textTheme.titleSmall,
+    // return Card(
+    //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+    //   margin: EdgeInsets.zero,
+    //   child: InkWell(
+    //     onTap: () => item.onTap(),
+    //     child: ListTile(
+    //       leading: Icon(item.icon),
+    //       title: Text(
+    //         item.title,
+    //         style: Theme.of(context).textTheme.titleMedium,
+    //       ),
+    //       subtitle: Text(
+    //         item.description,
+    //         style: Theme.of(context).textTheme.titleSmall,
+    //         overflow: TextOverflow.ellipsis,
+    //       ),
+    //     ),
+    //   ),
+    // );
+    return Row(
+      children: [
+        SizedBox(
+          width: 70,
+          height: 70,
+          child: Card(
+            child: InkWell(
+                borderRadius: BorderRadius.circular(18),
+                onTap: () => item.onTap(),
+                child: Icon(
+                  item.icon,
+                  size: 34,
+                )),
           ),
         ),
-      ),
+        SizedBox(
+          height: 70,
+          child: Card(
+            child: InkWell(
+              onTap: () => item.onTap(),
+              child: SizedBox(
+                width: 300,
+                child: ListTile(
+                  title: Text(
+                    item.title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  subtitle: Text(
+                    item.description,
+                    style: Theme.of(context).textTheme.titleSmall,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
